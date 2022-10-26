b44b17ecfab28b878d8b7afba8e0023204ca1011
kviiim <k82019@gmail.com>
2022-10-26 19:08:18 -0400

    add starter fiducial code

---
+++ tutorial_implementation/tutorial_implementation/find_marker.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+class FindMarker():
+    def __init__(self):
+        pass
+
+    def detector(self, image):
+        '''
+        Identify tag shaped items in a given image
+        '''
+        pass
+
+
+
+    def identifier(self, potential_tag):
+        '''
+        For a potential tag, identify it's fiducial ID or reject it
+        '''
+        pass
