3ac85631e00a2753d198b67ef18e277384fd25a4
kviiim <k82019@gmail.com>
2022-11-07 12:05:18 -0500

    fixed image path

--- segmentation_implementation/segmentation_implementation/test_segmentation.py
+++ segmentation_implementation/segmentation_implementation/test_segmentation.py
@@ -3,5 +3,5 @@ import cv2
 
 #import an image and run segmentation on it
 image_segmentation = image_segmentation.Segment_Image()
-image = cv2.imread('/home/kat/ros2_ws/src/fiducial_tracking/tutorial_implementation/tutorial_implementation/tags/sideways_t.png')
+image = cv2.imread('segmentation_implementation/segmentation_implementation/tags/sideways_t.png')
 image_segmentation.detector(image)
